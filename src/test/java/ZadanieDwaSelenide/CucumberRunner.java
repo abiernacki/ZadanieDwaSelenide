d3f2ecee556325bf42c47b1ff40b5109f4397d88
Adrian Biernacki <adrianbie86@gmail.com>
2022-01-08 22:15:23 +0100

    Cucmber raports

--- src/test/java/ZadanieDwaSelenide/CucumberRunner.java
+++ src/test/java/ZadanieDwaSelenide/CucumberRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/Cucumber/Features/zadanie-dwa.feature"},
-        plugin = {"pretty", "html:out"}
+        plugin = {"pretty", "html:out", "html:target/cucumber-html-report", "json:target/cucumber.json",
+                "pretty: target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
+                "junit:target/cucumber-results.xml"}
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 }
